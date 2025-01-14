cc4da83e35016795289b32bb6f5f32560e0920f5
David Lopes <155011706+DavidLopesTeixeira@users.noreply.github.com>
2025-01-14 14:28:40 -0300

    Exercicio mod-22 concluido

--- Mod-22-Exercicio-Streams/FiltroDeSexo-Cadastro-De-Individuos/src/br/com/davidlopes/cadastro_pessoas/Main.java
+++ Mod-22-Exercicio-Streams/FiltroDeSexo-Cadastro-De-Individuos/src/br/com/davidlopes/cadastro_pessoas/Main.java
@@ -1,7 +1,144 @@
 package br.com.davidlopes.cadastro_pessoas;
 
-public class Main {
-    public static void main(String[] args) {
+import br.com.davidlopes.cadastro_pessoas.model.Pessoa;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class Main {
+    private static List<Pessoa> pessoas = new ArrayList<>();
+
+    public static void main(String[] args) {
+        menu();
+    }
+
+    private static void menu() {
+        Scanner sc = new Scanner(System.in);
+        boolean isSair = false;
+        int op;
+
+        do {
+            System.out.println("Cadastrar Pessoas");
+            System.out.println("Digite 1 para cadastrar, 2 exibir pessoas cadastradas ou 3 para sair");
+            op = sc.nextInt();
+            sc.nextLine();
+
+            switch (op){
+                case 1:
+                    System.out.println("Cadastrar nova pessoa");
+                    cadastrarPessoas();
+                    break;
+                case 2:
+                    System.out.println("Exibir pessoas cadastradas");
+                    tratarTipos();
+                    break;
+                case 3:
+                    isSair = true;
+                    sc.close();
+                    System.out.println("Até logo...");
+                    break;
+                default:
+                    System.out.println("Opção inválida. Tente novamente.");
+            }
+
+        }while (!isSair);
+
+    }
+
+    private static void cadastrarPessoas() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Separados por virgula digite nome, sexo(M ou F), naturalidade(Brasil), idade");
+        String valores = sc.nextLine();
+        String[] dados = valores.split(",");
+        String nome = dados[0];
+        String sexo = dados[1].trim().toUpperCase();
+        String natura = dados[2];
+        try{
+            if(sexo.equals("M") || sexo.equals("F")){
+                Integer idade = Integer.parseInt(dados[3].trim());
+                if(sexo.equals("M")){
+                    sexo = "Masculino";
+                } else {
+                    sexo = "Feminino";
+                }
+                Pessoa pessoa = new Pessoa(dados[0].trim(), sexo, dados[2].trim(), idade);
+                pessoas.add(pessoa);
+
+                System.out.println(pessoa);
+                cadastrarNovo();
+
+            } else {
+                System.out.println("Digite M para masculino ou F para feminino");
+            }
+
+        }catch (Exception e){
+            System.out.println("Ocorreu um erro...");
+        }
+    }
+
+    private static void tratarTipos() {
+        if(pessoas.isEmpty()){
+            System.out.println("A lista está vazia! \n");
+            return;
+        }
+
+        boolean isSair = false;
+        List<Pessoa> mulheres = pessoas.stream()
+                .filter(p -> p.getSexo().equals("Feminino"))
+                .collect(Collectors.toList());
+
+        List<Pessoa> homens = pessoas.stream()
+                .filter(p -> p.getSexo().equals("Masculino"))
+                .collect(Collectors.toList());
+
+        Scanner sc = new Scanner(System.in);
+
+        do {
+            System.out.println("Selecione 0 para sair,  1 para todos, 2 para Mulheres e 3 para Homens:");
+            Integer op = sc.nextInt();
+            sc.nextLine();
+            switch (op){
+                case 1:
+                    System.out.println("Todas as pessoas cadastradas:");
+                    pessoas.forEach(p -> System.out.println(p));
+                    break;
+                case 2:
+                    System.out.println("Mulheres cadastradas:");
+                    mulheres.forEach(m -> System.out.println(m));
+                    break;
+                case 3:
+                    System.out.println("Homens cadastrados:");
+                    homens.forEach(h -> System.out.println(h));
+
+                case 0:
+                    System.out.println("Até logo...");
+                    menu();
+                    break;
+                default:
+                    System.out.println("Opção invalida!");
+            }
+
+        } while (!isSair);
+
+    }
+
+
+    private static void cadastrarNovo() {
+        Scanner sc = new Scanner(System.in);
+        boolean cadastrarNovo = false;
+        do {
+            System.out.println("Digite 1 para cadastrar outra ou 2 para sair");
+            Integer op = sc.nextInt();
+
+            if (op.equals(1)){
+                cadastrarPessoas();
+            } else{
+                menu();
+                cadastrarNovo = true;
+            }
+        }while (!cadastrarNovo);
     }
 }
--- Mod-22-Exercicio-Streams/FiltroDeSexo-Cadastro-De-Individuos/src/br/com/davidlopes/cadastro_pessoas/model/Pessoa.java
+++ Mod-22-Exercicio-Streams/FiltroDeSexo-Cadastro-De-Individuos/src/br/com/davidlopes/cadastro_pessoas/model/Pessoa.java
@@ -1,4 +1,79 @@
 package br.com.davidlopes.cadastro_pessoas.model;
 
 public class Pessoa {
+    private String nome;
+    private String sexo;
+    private String nacionalidade;
+    private Integer idade;
+    private Integer id;
+
+    private static int contadorId = 1;
+
+    public Pessoa(String nome, String sexo, String nacionalidade, Integer idade){
+        this.nome = nome;
+        this.sexo = sexo;
+        this.nacionalidade = nacionalidade;
+        this.idade = idade;
+        id = incrementId();
+    }
+
+    private int incrementId(){
+        return contadorId ++;
+    }
+
+    public Pessoa(){
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getNacionalidade() {
+        return nacionalidade;
+    }
+
+    public void setNacionalidade(String nacionalidade) {
+        this.nacionalidade = nacionalidade;
+    }
+
+    public Integer getIdade() {
+        return idade;
+    }
+
+    public void setIdade(Integer idade) {
+        this.idade = idade;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+
+
+    @Override
+    public String toString() {
+        return "Pessoa {" +
+                " Nome: " + nome +
+                " Sexo:" + sexo +
+                " Idade: " + idade +
+                " Pais: " + nacionalidade +
+                " ID: " + id +
+                " }";
+    }
 }
