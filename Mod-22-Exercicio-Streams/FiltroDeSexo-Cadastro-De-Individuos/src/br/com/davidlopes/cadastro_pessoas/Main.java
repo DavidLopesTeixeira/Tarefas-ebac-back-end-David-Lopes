995666bb74ec623bd2f53759c11ba2a469f4824b
David Lopes <155011706+DavidLopesTeixeira@users.noreply.github.com>
2025-01-14 10:53:45 -0300

    Commit inicial exercício mod-22

---
+++ Mod-22-Exercicio-Streams/FiltroDeSexo-Cadastro-De-Individuos/src/br/com/davidlopes/cadastro_pessoas/Main.java
@@ -0,0 +1,7 @@
+package br.com.davidlopes.cadastro_pessoas;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ Mod-22-Exercicio-Streams/FiltroDeSexo-Cadastro-De-Individuos/src/br/com/davidlopes/cadastro_pessoas/model/Pessoa.java
@@ -0,0 +1,4 @@
+package br.com.davidlopes.cadastro_pessoas.model;
+
+public class Pessoa {
+}
