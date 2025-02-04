8e289340c508ea829006e9c96657fd0819b2b560
David Lopes <155011706+DavidLopesTeixeira@users.noreply.github.com>
2025-02-04 11:43:42 -0300

    Delete Mod-33 directory

--- Mod-33/src/main/java/br/com/david/dao/IAccessoryDao.java
+++
@@ -1,7 +0,0 @@
-package br.com.david.dao;
-
-import br.com.david.domain.Accessory;
-
-public interface IAccessoryDao {
-	public Accessory register(Accessory accessory);
-}
--- Mod-33/src/main/java/br/com/david/dao/ICarDao.java
+++
@@ -1,7 +0,0 @@
-package br.com.david.dao;
-
-import br.com.david.domain.Car;
-
-public interface ICarDao {
-	public Car register(Car car);
-}
